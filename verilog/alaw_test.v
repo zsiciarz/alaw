94f323d91e34f255692233f421e57cb36848914d
Zbigniew Siciarz <antyqjon@gmail.com>
2010-12-02 17:30:38 +0100

    First working implementation of the testbench.

--- verilog/alaw_test.v
+++ verilog/alaw_test.v
@@ -1,12 +1,27 @@
 module alaw_test;
 
-wire [7:0] input_lin;
+reg [7:0] input_lin;
 wire [7:0] output_alaw;
 wire [7:0] output_decoded;
+reg [7:0] error;
 
-// input_lin -> [koder] -> output_alaw -> [dekoder] -> output_decoded
+// input_lin -> [coder] -> output_alaw -> [decoder] -> output_decoded
 alaw_coder coder(.input_lin(input_lin), .output_alaw(output_alaw));
 alaw_decoder decoder(.input_alaw(output_alaw), .output_lin(output_decoded));
+
+initial begin
+    $display("Running testbench...");
+    input_lin = 0;
+    //$monitor("%b ==> %b ==> %b", input_lin, output_alaw, output_decoded);
+    for (input_lin = 0; input_lin < 8'b11111111; input_lin = input_lin + 1) begin
+        #5 error = output_decoded & ~input_lin;
+        // is there any bit in error set?
+        if (|error) begin
+            $display("error at iteration #%d", input_lin);
+        end
+    end
+    $finish;
+end
 
 endmodule
 
