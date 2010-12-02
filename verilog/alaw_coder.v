6899f58fa63cb45cb1969b41d4fc8b38353c5a9e
Zbigniew Siciarz <antyqjon@gmail.com>
2010-12-02 16:53:36 +0100

    Placeholder always block.

--- verilog/alaw_coder.v
+++ verilog/alaw_coder.v
@@ -3,5 +3,9 @@ module alaw_coder(input_lin, output_alaw);
 input [7:0] input_lin;
 output [7:0] output_alaw;
 
+always @(input_lin) begin
+    // TODO: A-law coder algorithm goes here ;)
+end
+
 endmodule
 
