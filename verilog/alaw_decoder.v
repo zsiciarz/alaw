3ab6ac9bb70325d16e805c6317c36e915c002a92
Zbigniew Siciarz <antyqjon@gmail.com>
2010-12-02 16:54:20 +0100

    Added decoder module skeleton.

---
+++ verilog/alaw_decoder.v
@@ -0,0 +1,11 @@
+module alaw_decoder(input_alaw, output_lin);
+
+input [7:0] input_alaw;
+output [7:0] output_lin;
+
+always @(input_alaw) begin
+    // TODO: A-law decoder algorithm goes here ;)
+end
+
+endmodule
+
